dc0bf05ec4d9c46469e6dde2d3b4d0f24d9fbbfa
frozenca <frozenca91@gmail.com>
2022-08-01 09:37:55 +0900

    Revert "Support SIMD for MSVC, refactor type punning for custom alloc"
    This reverts commit b0945595a6ce846512036ec7096fc566713fa4da.

--- fc_disk_fixed_alloc.h
+++ fc_disk_fixed_alloc.h
@@ -15,9 +15,16 @@ namespace frozenca {
 
 template <typename T>
 class MemoryResourceFixed : public std::pmr::memory_resource {
-  T *pool_ = nullptr;
+  union Chunk {
+    Chunk *next_;
+    T object_;
+  };
+  static_assert(!(sizeof(Chunk) % sizeof(T)) && !(sizeof(Chunk) % sizeof(T *)),
+                "Chunk is not aligned");
+
+  Chunk *pool_ = nullptr;
   std::size_t pool_size_ = 0;
-  T *free_ = nullptr;
+  Chunk *free_ = nullptr;
 
 public:
   MemoryResourceFixed(unsigned char *pool_ptr, std::size_t pool_byte_size) {
@@ -28,21 +35,21 @@ public:
         (std::bit_cast<std::size_t>(pool_ptr) % sizeof(T *))) {
       throw std::invalid_argument("pool ptr is not aligned with T/T*");
     }
-    if (pool_byte_size < sizeof(T)) {
+    if (pool_byte_size < sizeof(Chunk)) {
+      std::cout << pool_byte_size << ' ' << sizeof(Chunk) << '\n';
       throw std::invalid_argument("pool byte size is too small");
     }
     if ((pool_byte_size % alignof(T)) || (pool_byte_size % sizeof(T *))) {
       throw std::invalid_argument("pool byte size is not aligned with T/T*");
     }
 
-    pool_ = reinterpret_cast<T *>(pool_ptr);
+    pool_ = reinterpret_cast<Chunk *>(pool_ptr);
     // size in chunks
-    pool_size_ = pool_byte_size / sizeof(T);
+    pool_size_ = pool_byte_size / sizeof(Chunk);
 
     auto curr_chunk = pool_;
     for (size_t i = 0; i < pool_size_; i++, curr_chunk++) {
-      *(reinterpret_cast<uint64_t *>(curr_chunk)) =
-          std::bit_cast<uint64_t>(curr_chunk + 1);
+      curr_chunk->next_ = curr_chunk + 1;
     }
     free_ = pool_;
   }
@@ -54,19 +61,23 @@ public:
 private:
   void *do_allocate([[maybe_unused]] std::size_t num_bytes,
                     [[maybe_unused]] std::size_t alignment) override {
+    assert((num_bytes % sizeof(Chunk)) == 0 &&
+           (alignment % alignof(Chunk)) == 0);
     if (free_ == pool_ + pool_size_) {
       throw std::invalid_argument("fixed allocator out of memory");
     } else {
       auto x = free_;
-      free_ = std::bit_cast<T *>(*(reinterpret_cast<uint64_t *>(x)));
+      free_ = x->next_;
       return reinterpret_cast<void *>(x);
     }
   }
 
   void do_deallocate(void *x, [[maybe_unused]] std::size_t num_bytes,
                      [[maybe_unused]] std::size_t alignment) override {
-    auto x_chunk = reinterpret_cast<T *>(x);
-    *(reinterpret_cast<uint64_t *>(x)) = std::bit_cast<uint64_t>(free_);
+    assert((num_bytes % sizeof(Chunk) == 0) &&
+           (alignment % alignof(Chunk)) == 0);
+    auto x_chunk = reinterpret_cast<Chunk *>(x);
+    x_chunk->next_ = free_;
     free_ = x_chunk;
   }
 
